aa2c28b23eae0f752dee64fcf1c3a90fae693979
acarril <acarril@princeton.edu>
2022-02-17 13:27:39 -0500

    add params

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import numpy as np
 import pulp
 
+# Parameters
+D = ("Mon", "Tue", "Wed")
+T = ("Morning", "Afternoon")
+G = ("A", "B")
+S = ("Math", "Lang")
+I = ("Alejandro", "Alvaro")
+
+m = np.zeros(len(D)*len(T)*len(G)*len(S))
+y = np.zeros(len(I)*len(G)*len(S))
+
+v_m = np.random.rand(len(D)*len(T)*len(G)*len(S))
+v_y = np.random.rand(len(I)*len(G)*len(S))
+
 # Constraints
 m = pulp.LpVariable('m', lowBound=0, upBound=None)
