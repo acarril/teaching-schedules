f356e7e7fbb3b80e057fcdc86a340d2d37a8d99a
Alejandro Robinson-Cortes <arobinson.cortes@gmail.com>
2022-04-19 18:37:46 +0100

    update blending problem (python)

--- tests/blending_problem.py
+++ tests/blending_problem.py
@@ -1,10 +1,12 @@
 # Simple PuLP example; source: https://coin-or.github.io/pulp/CaseStudies/a_blending_problem.html
 import pulp as p
 
-# Create the 'prob' variable to contain the problem data
+# Create the 'prob' variable to contain the problem data:
+# LpProblem("name of the problem","LpMinimize" or "LpMaximize")
 problem = p.LpProblem('WhiskasProblem', p.LpMinimize)
 
 # The 2 variables Beef and Chicken are created with a lower limit of zero
+# LpVariable("name of variable",lower bound, upper bound,"LpContinuous" or "LpInteger")
 x1 = p.LpVariable('ChickenPercent', 0, None, p.LpInteger)
 x2 = p.LpVariable('BeefPercent', 0)
 
@@ -18,8 +20,14 @@ problem += 0.080 * x1 + 0.100 * x2 >= 6.0, "FatRequirement"
 problem += 0.001 * x1 + 0.005 * x2 <= 2.0, "FibreRequirement"
 problem += 0.002 * x1 + 0.005 * x2 <= 0.4, "SaltRequirement"
 
+# The problem data is written to an .lp file
+problem.writeLP("WhiskasModel.lp")
+
 # The problem is solved using PuLP's choice of Solver
 problem.solve()
+
+# The status of the solution is printed to the screen
+print("Status:", p.LpStatus[problem.status])
 
 # Each of the variables is printed with it's resolved optimum value
 for var in problem.variables():
